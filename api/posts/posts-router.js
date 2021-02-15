15777f7fbce8ae08456186939cdef8fabaaedba3
Eric Della <ericdella13@gmail.com>
2021-02-14 20:41:27 -0800

    post posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,4 +35,23 @@ router.get("/api/posts/:id", (req, res) => {
     })
 })
 
+router.post("/api/posts", (req, res) => {
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            message: "Please provide title and contents for the post" 
+        })
+    }
+    
+    posts.insert(req.body)
+    .then((post) => {
+        res.status(201).json(post)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "There was an error while saving the post to the database"
+        })
+    })
+})
+
 module.exports = router 
